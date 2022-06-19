e13ff8910a76dc33805e53b055ee8fa47fbd3b89
Meilyn22 <samsonidoko3@byui.edu>
2022-06-18 20:48:30 -0600

    added swagger

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 
 router.use('/contacts', require('./contacts'))
-
+router.use('/', require('./swagger'))
 router.get("/", (req, res) => {
     res.send("Alexis Cook")
 })
---
+++ swagger.js
@@ -0,0 +1,14 @@
+const swaggerAutogen = require('swagger-autogen')();
+
+const doc = {
+  info: {
+    title: 'My API',
+    description: 'Temple API'
+  },
+  host: 'https://cse341node-samson.herokuapp.com/',
+  schemes: ['https']
+};
+
+const outputFile = './swagger.json';
+const endpointsFiles = ['./routes/index.js'];
+swaggerAutogen(outputFile, endpointsFiles, doc);
